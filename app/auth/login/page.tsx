d738451f1780c6a684dbd9216ef83887c7f0b4bd
eodeoddl <wlsdlsdmlrja@naver.com>
2024-10-02 20:45:43 +0900

    vercel deploy 에러수정

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,5 +1,10 @@
 import LoginForm from '@/components/auth/login-form';
+import { headers } from 'next/headers';
 
 export default function Page() {
-  return <LoginForm />;
+  const referer = headers().get('referer');
+  const redirectTo = referer
+    ? new URL(referer).pathname + new URL(referer).search
+    : '/';
+  return <LoginForm redirectTo={redirectTo} />;
 }
