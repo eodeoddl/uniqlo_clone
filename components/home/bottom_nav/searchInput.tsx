f3b5439a2bba3e2f7eb6ba6fe59ce2feb72e6cc5
eodeoddl <wlsdlsdmlrja@naver.com>
2024-10-11 19:10:22 +0900

    검색 인풋 로직수정

--- components/home/bottom_nav/searchInput.tsx
+++ components/home/bottom_nav/searchInput.tsx
@@ -1,45 +1,42 @@
 'use client';
 import { Search } from 'lucide-react';
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { useEffect, useRef, useState } from 'react';
 
 export default function SearchInput() {
   const inputRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
-  const pathname = usePathname();
   const { replace } = useRouter();
   const [inputValue, setInputValue] = useState(
     searchParams.get('query')?.toString() || ''
   );
 
-  console.log('input component ');
-
-  const handleSearch = (term: string) => {
+  const handleSubmit = () => {
     const params = new URLSearchParams();
-    term ? params.set('query', term) : params.delete('query');
-    replace(`${pathname}?${params.toString()}`);
+    inputValue ? params.set('query', inputValue) : params.delete('query');
+    replace(`/search?${params.toString()}`);
   };
 
-  // useEffect(() => {
-  //   const query = searchParams.get('query')?.toString() || '';
-  //   console.log('stststst', query);
-  //   setInputValue(query);
-  // }, [searchParams.get('query')]);
+  useEffect(() => {
+    const query = searchParams.get('query')?.toString() || '';
+    setInputValue(query);
+  }, [searchParams.get('query')]);
 
   return (
     <div className='flex items-center w-3/4 md:w-1/2 lg:w-96 h-10 rounded-lg border-2 p-2 mx-auto focus-within:border-blue-500'>
       <Search
         size={20}
         className='w-[10%] h-full cursor-pointer'
-        onClick={() => inputRef.current?.focus()}
+        onClick={() => handleSubmit()}
       />
       <input
         type='text'
         className='w-[90%] outline-none bg-transparent'
         placeholder='키워드로 검색'
         ref={inputRef}
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={(e) => setInputValue(e.target.value)}
         value={inputValue}
+        onKeyDown={(e) => e.key === 'Enter' && handleSubmit()}
       />
     </div>
   );
