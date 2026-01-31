7e966e86eb93ed3ac8922fb515e732d5c8716860
lki <>
2026-01-31 22:57:26 +0900

    collection Page 수정

--- app/(protected)/account/profile/@tabs/collections/page.tsx
+++ app/(protected)/account/profile/@tabs/collections/page.tsx
@@ -1,9 +1,15 @@
-import { getAllCollectionsByUser } from '@/actions/handleCollection';
-import { auth } from '@/auth';
 import UserCollections from '@/components/account/collections';
+import { auth } from '@/auth';
+import { getAllCollectionsByUser } from '@/actions/handleCollection';
+import { redirect } from 'next/navigation';
+
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   const session = await auth();
+  if (!session) {
+    redirect('/modal/auth/login'); // 여기를 모달 로그인 라우트로
+  }
   const collections = await getAllCollectionsByUser(session?.user.id!);
   return <UserCollections collections={collections} />;
 }
