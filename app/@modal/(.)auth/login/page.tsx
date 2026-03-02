79e9889f8e0845222e8e6cba1b33a46e30b9f946
lki <>
2026-03-02 17:32:38 +0900

    오류수정

--- app/@modal/(.)auth/login/page.tsx
+++ app/@modal/(.)auth/login/page.tsx
@@ -1,17 +1,15 @@
-import LoginForm from '@/components/auth/login-form';
 import {
   AlertDialog,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
+
+import LoginForm from '@/components/auth/login-form';
 import { headers } from 'next/headers';
 
 export default async function Modal() {
   const referer = headers().get('referer');
-  const redirectTo = referer
-    ? new URL(referer).pathname + new URL(referer).search
-    : '/';
 
   return (
     <AlertDialog defaultOpen>
@@ -20,7 +18,7 @@ export default async function Modal() {
         <AlertDialogDescription className='hidden'>
           로그인을하고 더많은 기능을 이용해 보세요!
         </AlertDialogDescription>
-        <LoginForm redirectTo={redirectTo} />
+        <LoginForm />
       </AlertDialogContent>
     </AlertDialog>
   );
